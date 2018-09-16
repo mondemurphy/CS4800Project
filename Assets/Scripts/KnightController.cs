c0b473bbc0f8443809ee042621c128e5f85d4502
mondemurphy <41589673+mondemurphy@users.noreply.github.com>
2018-09-15 18:43:01 -0600

    Started Another Test Scene
    I made a new PlayerController called KnightController that works better for a top down game.

---
+++ Assets/Scripts/KnightController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : MonoBehaviour {
+
+    // Normal Movements Variables
+    private float walkSpeed;
+    private float sprintSpeed;
+    private float curSpeed;
+    private float maxSpeed;
+    private Rigidbody2D rb2d;
+    Vector3 move = Vector3.zero;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    private CharacterStat plStat;
+
+    void Start()
+    {
+        plStat = GetComponent<CharacterStat>();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        walkSpeed = (float)(plStat.Speed + (plStat.Agility / 5));
+        sprintSpeed = walkSpeed + (walkSpeed / 2);
+
+    }
+
+    void Update()
+    {
+        curSpeed = walkSpeed;
+        maxSpeed = curSpeed;
+        move = transform.position;
+
+        // Move senteces
+        rb2d.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("knightAttack");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("knightBlock");
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            anim.SetTrigger("knightUnBlock");
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            move.x += Input.GetAxis("Horizontal") * 0.1f;
+            rb2d.AddForce(Vector2.right * curSpeed);
+            transform.position = move;
+        }
+
+        if (Input.GetButtonDown("Horizontal") || Input.GetButton("Horizontal"))
+        {
+            Vector3 charDir = Vector3.zero;
+            charDir.x = Input.GetAxis("Horizontal");
+            if (charDir.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            move.y += Input.GetAxis("Vertical") * 0.1f;
+            rb2d.AddForce(Vector2.up * curSpeed);
+            transform.position = move;
+        }
+
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+        {
+            anim.SetTrigger("knightWalk");
+        }
+
+        if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
+        {
+            anim.SetTrigger("knightStop");
+        }
+    }
+}
