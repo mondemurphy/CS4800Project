a7176558c5e228f64d672a254039fb6ae1d7bfd1
mondemurphy <41589673+mondemurphy@users.noreply.github.com>
2018-09-21 18:23:23 -0600

    Added enemy animations and AI

---
+++ Assets/Scripts/FrogController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+
+    private Transform target;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target.position.x > transform.position.x)
+            sprite.flipX = true;
+        else
+            sprite.flipX = false;
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            anim.SetBool("Walking", true);
+        }
+    }
+}
