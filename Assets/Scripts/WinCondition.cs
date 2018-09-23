2ccd19828b8b81c2b0ea46752f4c8fa5bf3974d3
mondemurphy <41589673+mondemurphy@users.noreply.github.com>
2018-09-23 13:52:34 -0600

    Updated some files to get ready to turn in for my first progress report

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class WinCondition : MonoBehaviour {
 
     private Animator anim;
+    private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +18,8 @@ public class WinCondition : MonoBehaviour {
         if (GameObject.Find("Enemies").transform.childCount == 0)
         {
             anim.SetBool("Win", true);
+            rb2d.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
         }
     }
 }
+ 
